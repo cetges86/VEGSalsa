3bb3a46fb1ecea2a1004f87fce86c7399aced7c8
Christopher Etges <c.etges86@gmail.com>
2020-06-10 10:56:37 -0600

    update captcha/contact form

---
+++ veg-site/no-react/contact.php
@@ -0,0 +1,81 @@
+<?php
+    function post_captcha($user_response) {
+        $fields_string = '';
+        $fields = array(
+            'secret' => '6Lf1uaIZAAAAAIDMgdFk2CHfBztha6p7Kso0qiWu',
+            'response' => $user_response
+        );
+        foreach($fields as $key=>$value)
+        $fields_string .= $key . '=' . $value . '&';
+        $fields_string = rtrim($fields_string, '&');
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
+        curl_setopt($ch, CURLOPT_POST, count($fields));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, True);
+
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($result, true);
+    }
+
+    // Call the function post_captcha
+    $res = post_captcha($_POST['g-recaptcha-response']);
+
+    if (!$res['success']) {
+        // What happens when the CAPTCHA wasn't checked
+        echo '<p>Please go back and make sure you check the security CAPTCHA box.</p><br>';
+    } else {
+        // If CAPTCHA is successfully completed...
+
+ 
+        $from = '<c.etges86@gmail.com>';
+        $sendTo = '<c.etges86@gmail.com>';
+        $subject = 'New message from contact form';
+        $fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
+        $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+        $errorMessage = 'There was an error while submitting the form. Please try again later';
+
+        // let's do the sending
+
+        try
+        {
+            $emailText = "You have new message from contact form\n=============================\n";
+
+            foreach ($_POST as $key => $value) {
+
+                if (isset($fields[$key])) {
+                    $emailText .= "$fields[$key]: $value\n";
+                }
+            }
+
+            $headers = array('Content-Type: text/plain; charset="UTF-8";',
+                'From: ' . $from,
+                'Reply-To: ' . $from,
+                'Return-Path: ' . $from,
+            );
+            
+            mail($sendTo, $subject, $emailText, implode("\n", $headers));
+
+            $responseArray = array('type' => 'success', 'message' => $okMessage);
+        }
+        catch (\Exception $e)
+        {
+            $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+        }
+
+        if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+            $encoded = json_encode($responseArray);
+
+            header('Content-Type: application/json');
+
+            echo $encoded;
+        }
+        else {
+            echo $responseArray['message'];
+        }
+
+            }
+        ?>
